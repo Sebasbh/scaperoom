51c2ce656a040ed835edeb239139360422c846c9
JesCab29 <jesusacs2906@gmail.com>
2023-06-14 14:27:18 +0200

    cambios finales

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ const codigoCorrecto = ['rojo', 'negro', 'azul', 'amarillo'];
 
 // Verificar si el código ingresado es correcto
 function verificarCodigo() {
-  const codigoIngresado = obtenerCodigoIngresado();
+    const codigoIngresado = obtenerCodigoIngresado();
 
-  if (codigoIngresado.length === codigoCorrecto.length && codigoIngresado.every((color, index) => color === codigoCorrecto[index])) {
-    mostrarMensaje("¡Código correcto!");
-  } else {
-    mostrarMensaje("Código incorrecto. Intenta nuevamente.");
-  }
+    if (codigoIngresado.length === codigoCorrecto.length && codigoIngresado.every((color, index) => color === codigoCorrecto[index])) {
+        mostrarMensaje("¡Código correcto!");
+    } else {
+        mostrarMensaje("Código incorrecto. Intenta nuevamente.");
+    }
 }
 
 // Obtener el código ingresado por los jugadores
 function obtenerCodigoIngresado() {
-  const codigoInput = document.getElementById('code-input').value;
-  return codigoInput.split(' ');
+    const codigoInput = document.getElementById('code-input').value;
+    return codigoInput.split(' ');
 }
 
 // Mostrar mensaje en la pantalla
 function mostrarMensaje(mensaje) {
-  const mensajeElement = document.getElementById('message');
-  mensajeElement.textContent = mensaje;
+    const mensajeElement = document.getElementById('mensaje');
+    mensajeElement.textContent = mensaje;
 }
 
 // Evento al hacer clic en el botón de enviar
